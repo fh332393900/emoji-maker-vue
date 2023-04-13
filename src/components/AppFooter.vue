8c70c0db5fbf84667b3d411a0f97763efb9bf5b1
fenghang <hang.feng@nx-engine.com>
2023-04-13 17:07:39 +0800

    修改

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,7 @@
   <footer class="flex justify-center align-center">
     <div>Steven Feng</div>
     <div>|</div>
-    <a href="">Github</a>
+    <a href="https://github.com/fh332393900/emoji-maker-vue" target="_blank">Github</a>
   </footer>
 </template>
 
@@ -17,5 +17,6 @@ footer {
 footer div,a {
   font-weight: 600;
   margin-right: 14px;
+  color: rgba(167,139,250);
 }
 </style>
