f544391bc6ac3e7d64ca342e360dab99da6df810
fenghang <hang.feng@nx-engine.com>
2023-04-27 09:46:54 +0800

    修改server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express  = require('express');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 8081;
 
 app.listen(PORT, () => {
   console.info('App running: http://127.0.0.1:' + PORT);
