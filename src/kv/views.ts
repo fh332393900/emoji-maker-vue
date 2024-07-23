27ea5e0a12e285b38079fbaaef8e8d7e0c80d381
fenghang <332393900@qq.com>
2024-07-23 21:21:15 +0800

    feat: 修改kv

--- src/kv/views.ts
+++ src/kv/views.ts
@@ -2,8 +2,6 @@ import { createClient } from "@vercel/kv";
 
 const KV_REST_API_URL = import.meta.env.VITE_KV_REST_API_URL;
 const KV_REST_API_TOKEN = import.meta.env.VITE_KV_REST_API_TOKEN;
-console.log(import.meta.env);
-console.log(process.env);
 const kv = createClient({
   url: KV_REST_API_URL,
   token: KV_REST_API_TOKEN,
